7457a282650bef24037d88b4127bbb957cbed896
yotamsil <yotams2@mail.tau.ac.il>
2024-02-03 18:29:09 +0200

    test

--- models.py
+++ models.py
@@ -42,4 +42,14 @@ def get_xception_based_model() -> nn.Module:
     classification head stated in the exercise.
     """
     """INSERT YOUR CODE HERE, overrun return."""
-    return SimpleNet()
+    custom_network = build_xception_backbone(True)
+    custom_network.fc = nn.Sequential(
+          nn.Linear(2048, 1000),
+          nn.ReLU(),
+          nn.Linear(1000, 256),
+          nn.ReLU(),
+          nn.Linear(256, 64),
+          nn.ReLU(),
+          nn.Linear(64, 2)
+        )
+    return custom_network
